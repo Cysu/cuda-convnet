Correct the cost computation part

convdata.py
@@ -22,6 +22,8 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import os
+import subprocess
 from time import time
 
 from data import *
@@ -58,16 +60,6 @@ class CroppedOnlineMultiTaskDataProvider(LabeledDataProvider):
 
         # datadic['data'] = n.require(datadic['data'], dtype=n.single, requirements='C')
         datadic['data'] = datadic['data'].astype(n.single) # 30x faster
-
-        # Set all-zero multi-label samples to -1
-        # TODO: Should be done when generating data batches
-        for i, l in enumerate(datadic['labels']):
-            if self.label_types[i] == 'multi-class':
-                continue
-            col_sum = l.sum(axis=0)
-            bad_inds = n.where(col_sum == 0)[0]
-            l = l.astype(n.float32)
-            l[:, bad_inds] = -1
 
         datadic['labels'] = [n.require(n.tile(L, (1, self.data_mult)), dtype=n.single, requirements='C') for L in datadic['labels']]
 
@@ -181,6 +173,31 @@ class CroppedOnlineDataProvider(LabeledDataProvider):
                 if nr.randint(2) == 0: # also flip the image with 50% probability
                     pic = pic[:,:,::-1]
                 target[:,c] = pic.reshape((self.get_data_dims(),))
+
+class HDFSCroppedOnlineMultiTaskDataProvider(CroppedOnlineMultiTaskDataProvider):
+    # TODO: It is just for getting hdfs file faster. Fix this part later.
+
+    def get_batch(self, batch_num):
+        tmpname = '/home/vis/xiaotong/baidu/build/convnet/cache/{}'.format(randint(123456789))
+        cmd = ['/home/vis/xiaotong/from_liaojie/hadoop/bin/hadoop', 'fs', '-get']
+        cmd.append(self.get_data_file_name(batch_num))
+        cmd.append(tmpname)
+        subprocess.call(cmd)
+        dic = unpickle(tmpname)
+        os.remove(tmpname)
+        return dic
+
+    @staticmethod
+    def get_batch_meta(data_dir):
+        tmpname = '/home/vis/xiaotong/baidu/build/convnet/cache/{}'.format(randint(123456789))
+        cmd = ['/home/vis/xiaotong/from_liaojie/hadoop/bin/hadoop', 'fs', '-get']
+        cmd.append(os.path.join(data_dir, 'batches.meta'))
+        cmd.append(tmpname)
+        subprocess.call(cmd)
+        dic = unpickle(tmpname)
+        os.remove(tmpname)
+        return dic
+
 
 class CIFARDataProvider(LabeledMemoryDataProvider):
     def __init__(self, data_dir, batch_range, init_epoch=1, init_batchnum=None, dp_params={}, test=False):

layer.py
@@ -859,6 +859,7 @@ class CondProbLayerParser(LayerWithInputParser):
                 dic['weightsInc'] += [n.zeros_like(dic['weights'][i])]
                 print "Layer '%s[%d]' initialized weight matrices from function %s" % (dic['name'], i, dic['initWFunc'])
         else:
+            assert len(dic['inputs']) == 1
             for i in xrange(len(dic['inputs'])):
                 if dic['weightSourceLayerIndices'][i] >= 0: # Shared weight matrix
                     src_layer = self.prev_layers[dic['weightSourceLayerIndices'][i]] if dic['weightSourceLayerIndices'][i] < len(self.prev_layers) else dic
@@ -871,27 +872,29 @@ class CondProbLayerParser(LayerWithInputParser):
                         raise LayerParsingError("Layer '%s[%d]': weight matrix is not a probability matrix" % (dic['name'], i))
                     print "Layer '%s' initialized weight matrix %d from %s" % (dic['name'], i, dic['weightSource'][i])
                 else:
-                    if initW[i] == 'random':
+                    if initW == 'random':
                         dic['weights'] += [n.array(nr.rand(rows[i], cols[i]), dtype=n.single, order=order)]
                         dic['weights'][i] /= dic['weights'][i].sum(axis=0)
-                    elif initW[i] == 'identity':
+                    elif initW == 'identity':
                         if rows[i] != cols[i]:
                             raise LayerParsingError("Layer '%s[%d]': weight matrix cannot be set to Identity matrix since it is not square" % (dic['name'], i))
                         dic['weights'] += [n.array(n.identity(rows[i]), dtype=n.single, order=order)]
                     else:
-                        raise LayerParsingError("Layer '%s[%d]': weight matrix initW error; should be either random or identity" % (dic['name'], i))
+                        try:
+                            dic['weights'] += [n.matrix(initW, dtype=n.single)]
+                        except:
+                            raise LayerParsingError("Layer '%s[%d]': weight matrix initW error; should be either random or identity" % (dic['name'], i))
                     dic['weightsInc'] += [n.zeros_like(dic['weights'][i])]
         
     def parse(self, name, mcp, prev_layers, model):
         dic = LayerWithInputParser.parse(self, name, mcp, prev_layers, model)
         dic['requiresParams'] = True
         dic['gradConsumer'] = True
-        dic['initW'] = mcp.safe_get_list(name, 'initW', default="random")
+        dic['initW'] = mcp.safe_get(name, 'initW', default="random")
         dic['initWFunc'] = mcp.safe_get(name, 'initWFunc', default="")
         # Find shared weight matrices
         
-        dic['weightSource'] = mcp.safe_get_list(name, 'weightSource', default=[''] * len(dic['inputs']))
-        self.verify_num_params(['initW', 'weightSource'])
+        dic['weightSource'] = ['']
         
         prev_names = map(lambda x: x['name'], prev_layers)
         dic['weightSourceLayerIndices'] = []